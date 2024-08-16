63a143f353de37fbf388c2b2bdb1c8fc01b0faac
Marius Susanu <marius@susanu.net>
2024-08-16 14:45:52 +0300

    Update boolean.blade.php

--- src/resources/views/crud/fields/boolean.blade.php
+++ src/resources/views/crud/fields/boolean.blade.php
@@ -1,1 +1,7 @@
-@include('crud::fields.checkbox')
+@php
+    $field['value'] = old_empty_or_null($field['name'], '') ?? $field['value'] ?? $field['default'] ?? '0';
+    $field['options'][0] ??= Lang::has('backpack::crud.no') ? trans('backpack::crud.no') : 'No';
+    $field['options'][1] ??= Lang::has('backpack::crud.yes') ? trans('backpack::crud.yes') : 'Yes';
+@endphp
+
+@include('crud::fields.select_from_array')
